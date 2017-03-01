af081c88f9653591969253b1abc9bc919a588baa
ghv1293 <harshagunduboina@gmail.com>
2017-03-01 14:23:07 -0600

    Add files via upload

---
+++ Login.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" type="text/css" href="css/login.css">
+</head>
+<body>
+<div>
+    <nav class="navbar navbar-inverse">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <a class="navbar-brand" href="index.html">✌ GAMERS GUIDE ✌</a>
+            </div>
+            <ul class="nav navbar-nav">
+                <li class="active"><a href="index.html">Home</a></li>
+            </ul>
+        </div>
+    </nav>
+    <form>
+        <div class="input-groups">
+            <div class="input-group">
+                <input id="username" type="text" class="form-control" name="username" placeholder="Username">
+                <span class="input-group-addon"><i class="glyphicon glyphicon-bishop"></i></span>
+            </div>
+            <div class="input-group">
+                <input id="password" type="password" class="form-control" name="password" placeholder="Password">
+                <span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
+            </div>
+            <div class="input-group">
+                <button type="button" class="btn btn-success">Login</button>
+            </div>
+        </div>
+    </form>
+</div>
+<script type="text/javascript" src="js/jquery-3.1.1.min.js"></script>
+</body>
+</html>
